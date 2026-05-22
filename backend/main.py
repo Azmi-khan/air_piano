fb32cb14f0e775abfbf9cb26bdb72d84fecc64ed
Azmi_khan <azmikhan2023@gmail.com>
2026-05-22 21:03:18 +0200

    made changes to the backend code

--- backend/main.py
+++ backend/main.py
@@ -26,11 +26,12 @@ hands = mp_hands.Hands(
 def home_check():
     """checking if the server is running"""
     return {"message": "german Server is running"}
+
+
 @app.websocket("/ws/process")
 async def websocket_endpoint(websocket: WebSocket):
-    """websocket test"""
     await websocket.accept()
-    print("websocket accepted")
+    print("🚀 Air Piano session connected!")
 
     try:
         while True:
@@ -39,25 +40,45 @@ async def websocket_endpoint(websocket: WebSocket):
             frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
             if frame is None:
                 continue
+
             rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             results = hands.process(rgb_frame)
-            response_data = {"detected": False, "x": 0, "y": 0, "drawing": False}
+
+
+            response_data = {"detected": False, "x": 0, "y": 0, "note": None}
+
             if results.multi_hand_landmarks:
                 hand_landmarks = results.multi_hand_landmarks[0]
                 response_data["detected"] = True
+
                 index_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
                 thumb_tip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
+
                 response_data["x"] = index_tip.x
                 response_data["y"] = index_tip.y
+
+
                 distance = np.sqrt(
                     (index_tip.x - thumb_tip.x) ** 2 +
                     (index_tip.y - thumb_tip.y) ** 2 +
                     (index_tip.z - thumb_tip.z) ** 2
                 )
+
+
                 if distance < 0.06:
-                    response_data["drawing"]  = True
+
+                    if index_tip.y < 0.20:
+                        if 0.0 <= index_tip.x < 0.25:
+                            response_data["note"] = "C4"
+                        elif 0.25 <= index_tip.x < 0.50:
+                            response_data["note"] = "D4"
+                        elif 0.50 <= index_tip.x < 0.75:
+                            response_data["note"] = "E4"
+                        elif 0.75 <= index_tip.x <= 1.0:
+                            response_data["note"] = "F4"
+
             await websocket.send_json(response_data)
     except WebSocketDisconnect:
-        print("websocket disconnected")
+        print("❌ Air Piano disconnected.")
     except Exception as e:
-        print(f"system logic exception: {e}")
+        print(f"Engine exception: {e}")
